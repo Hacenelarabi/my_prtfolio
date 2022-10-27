2c68de28c87f6066fbecf11d2246caf294862058
hacene <larabi1603@gmail.com>
2022-10-27 11:43:11 -0400

    add javascripy

--- file.js
+++ file.js
@@ -3,11 +3,16 @@ const card1 = document.getElementById("card1");
 
 
 
-function showCard(e){
+/*function showCard(e){
     e.preventDefault();
     card0.innerText = "this is my project";
 }
-card0.addEventListener('click', showCard);
+card0.addEventListener('click', showCard);*/
+
+function onClick(x){
+    x.style.color = "blue";
+}
+
 
 
 
