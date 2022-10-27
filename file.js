4947df365ec7e2fca61ed1676d131ccb1d1dd46d
hacene <larabi1603@gmail.com>
2022-10-27 10:43:43 -0400

    modify the page

---
+++ file.js
@@ -0,0 +1,13 @@
+const card0 = document.getElementById("card0");
+const card1 = document.getElementById("card1");
+
+
+
+function showCard(e){
+    e.preventDefault();
+    card0.innerText = "this is my project";
+}
+card0.addEventListener('click', showCard);
+
+
+
